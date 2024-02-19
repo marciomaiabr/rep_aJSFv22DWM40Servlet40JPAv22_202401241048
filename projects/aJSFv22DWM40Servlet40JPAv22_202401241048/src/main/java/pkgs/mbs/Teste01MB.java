0019da6bec9c86d701665e41fad713c18e405182
marciomaiabr <marcio.luis.maia@gmail.com>
2024-02-19 07:57:41 -0400

    Testing ajax with simple data

--- projects/aJSFv22DWM40Servlet40JPAv22_202401241048/src/main/java/pkgs/mbs/Teste01MB.java
+++ projects/aJSFv22DWM40Servlet40JPAv22_202401241048/src/main/java/pkgs/mbs/Teste01MB.java
@@ -3,66 +3,116 @@ package pkgs.mbs;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.servlet.http.HttpServletRequest;
 
-import pkgs.models.Empresa;
-import pkgs.utils.JPAUtil;
+import pkgs.utils.JSFUtil;
 
 @ManagedBean
 public class Teste01MB {
 
+	static {
+		System.out.println("Teste01MB.static");
+		System.out.println("[JSFUtil.getProjectStage()=" + JSFUtil.getProjectStage() + "]");
+	}
+
 	public Teste01MB() {
-		System.out.println("Teste01MB.Teste01MB()");
+		System.out.println("Teste01MB.()");
 	}
 
 	@PostConstruct
 	public void postConstruct() {
 		System.out.println("Teste01MB.postConstruct()");
+
+		souts();
 	}
 
-	public long getVersion() {
-		return 202401241130l;
+	private String texto01;
+	private String texto02;
+	private String texto03;
+	private String texto04;
+
+	public String getTexto01() {
+		return texto01;
 	}
 
-	public void testDB() {
-		System.out.println("Teste01MB.testDB()");
-		EntityManagerFactory emf = null;
+	public void setTexto01(String texto01) {
+		this.texto01 = texto01;
+	}
 
-		emf = JPAUtil.criaEntityManagerFactoryWithCreateNone();
+	public String getTexto02() {
+		return texto02;
+	}
 
-		if(emf != null) {
-			EntityManager em = null;
+	public void setTexto02(String texto02) {
+		this.texto02 = texto02;
+	}
 
-			try {
-				em = emf.createEntityManager();
+	public String getTexto03() {
+		return texto03;
+	}
 
-			    CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-			    CriteriaQuery<Empresa> criteriaQuery = criteriaBuilder.createQuery(Empresa.class);
-			    Root<Empresa> root = criteriaQuery.from(Empresa.class);
-			    criteriaQuery.select(root);
-			    em.createQuery(criteriaQuery).getResultList().forEach(System.out::println);
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					em.close();
-				} catch (Exception e) {
-				}
-				try {
-					emf.close();
-				} catch (Exception e) {
-				}
-			}
-		}
+	public void setTexto03(String texto03) {
+		this.texto03 = texto03;
+	}
+
+	public String getTexto04() {
+		return texto04;
+	}
+
+	public void setTexto04(String texto04) {
+		this.texto04 = texto04;
+	}
+
+	private void souts() {
+		System.out.println("     " + "[this.texto01=" + this.texto01 + "]");
+		System.out.println("     " + "[this.texto02=" + this.texto02 + "]");
+		System.out.println("     " + "[this.texto03=" + this.texto03 + "]");
+		System.out.println("     " + "[this.texto04=" + this.texto04 + "]");
+	}
+
+	public void methodViewAction() {
+		System.out.println("Teste01MB.methodViewAction()");
+
+		souts();
+	}
+
+	public void commandButtonTexto01Click() {
+		System.out.println("Teste01MB.commandButtonTexto01Click()");
+
+		souts();
+	}
+
+	public void commandButtonTexto01AjaxListener() {
+		System.out.println("Teste01MB.commandButtonTexto01AjaxListener()");
+
+		souts();
+	}
+
+	public void commandButtonTexto01ActionListener() {
+		System.out.println("Teste01MB.commandButtonTexto01ActionListener()");
+
+		souts();
+	}
+
+	public void reload() {
+		System.out.println("Teste01MB.reload()");
+
+		souts();
+	}
+
+	public void confirmar() {
+		System.out.println("Teste01MB.confirmar()");
+
+		souts();
 	}
 
 	@PreDestroy
 	public void preDestroy() {
 		System.out.println("Teste01MB.preDestroy()");
+
+		souts();
 	}
 
 }
--- projects/aJSFv22DWM40Servlet40JPAv22_202401241048/src/main/java/pkgs/servletFilters/Teste01ServletFilter.java
+++ projects/aJSFv22DWM40Servlet40JPAv22_202401241048/src/main/java/pkgs/servletFilters/Teste01ServletFilter.java
@@ -18,8 +18,6 @@ import pkgs.utils.JPAUtil;
 @WebFilter(servletNames = { "Faces Servlet" })
 public class Teste01ServletFilter implements Filter {
 
-	private EntityManagerFactory entityManagerFactory;
-
 	public Teste01ServletFilter() {
 		System.out.println("Teste01ServletFilter.Teste01ServletFilter()");
 	}
@@ -27,51 +25,18 @@ public class Teste01ServletFilter implements Filter {
 	@Override
 	public void init(FilterConfig config) throws ServletException {
 		System.out.println("Teste01ServletFilter.init()");
-		entityManagerFactory = JPAUtil.criaEntityManagerFactoryWithCreateNone();
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		System.out.println("Teste01ServletFilter.doFilter()");
-
-		EntityManager entityManager = null;
-		EntityTransaction et = null;
-
-		try {
-			entityManager = entityManagerFactory.createEntityManager();
-			et = entityManager.getTransaction();
-			et.begin();
-
-			request.setAttribute("entityManager", entityManager);
-
-			chain.doFilter(request, response);
-
-			et.commit();
-		} catch (Exception exception1) {
-			System.out.println("exception1...");
-			exception1.printStackTrace();
-			try {
-				et.rollback();
-			} catch (Exception e) {
-			}
-		} finally {
-			try {
-				entityManager.close();
-			} catch (Exception e) {
-			}
-		}
-
-		System.out.println("/Teste01ServletFilter.doFilter()");
+		chain.doFilter(request, response);
 	}
 
 	@Override
 	public void destroy() {
 		System.out.println("Teste01ServletFilter.destroy()");
-		try {
-			entityManagerFactory.close();
-		} catch (Exception e) {
-		}
 	}
 
 }
