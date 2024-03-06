99937e7c1e105961fc0eaf44760c38f9a362afa9
marciomaiabr <marcio.luis.maia@gmail.com>
2024-03-06 09:28:51 -0400

    fixed initial data

--- projects/aJSFv22DWM40Servlet40JPAv22_202401241048/src/main/resources/META-INF/sqls/initial/dml/InsertUsuario_GrupoUsuario.sql
+++ projects/aJSFv22DWM40Servlet40JPAv22_202401241048/src/main/resources/META-INF/sqls/initial/dml/InsertUsuario_GrupoUsuario.sql
@@ -2,6 +2,8 @@
 Insert INTO usuario_grupousuario (usuarios_id, gruposUsuario_id) VALUES (1, 1);
 
 Insert INTO usuario_grupousuario (usuarios_id, gruposUsuario_id) VALUES (2, 2);
-Insert INTO usuario_grupousuario (usuarios_id, gruposUsuario_id) VALUES (2, 3);
 
 Insert INTO usuario_grupousuario (usuarios_id, gruposUsuario_id) VALUES (3, 3);
+
+Insert INTO usuario_grupousuario (usuarios_id, gruposUsuario_id) VALUES (4, 2);
+Insert INTO usuario_grupousuario (usuarios_id, gruposUsuario_id) VALUES (4, 3);
