6de42436cf27b1c0a37db70bda416979fd41b571
marciomaiabr <marcio.luis.maia@gmail.com>
2024-03-10 08:09:30 -0400

    Criando um provedor de autenticação customizado

---
+++ projects/aJSFv22DWM40Servlet40JPAv22_202401241048/src/main/java/pkgs/securitys/UsuarioLogadoDetailsService.java
@@ -0,0 +1,41 @@
+package pkgs.securitys;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import pkgs.models.Usuario;
+import pkgs.repositoryEspec.UsuarioRepositoryEspec;
+import pkgs.utils.CDIServiceLocator;
+
+public class UsuarioLogadoDetailsService implements Serializable , UserDetailsService {
+
+	private static final long serialVersionUID = 20240310070601L;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		UsuarioRepositoryEspec ur = CDIServiceLocator.getBean(UsuarioRepositoryEspec.class);
+		Usuario usuario = ur.porEmail(email);
+
+		if (usuario != null) {
+			UserDetails userDetails = new UsuarioLogado(usuario, getAuthorities(usuario));
+			return userDetails;
+		}
+
+		return null;
+	}
+
+	private Collection<? extends GrantedAuthority> getAuthorities(Usuario usuario) {
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		usuario.getGruposUsuario().forEach(gu -> {authorities.add(new SimpleGrantedAuthority(gu.getNome().toUpperCase()));});
+		return authorities;
+	}
+
+}
