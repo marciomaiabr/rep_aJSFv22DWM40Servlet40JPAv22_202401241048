d024f5ab9e235b197ca40541b653cfcf651e44d4
marciomaiabr <marcio.luis.maia@gmail.com>
2024-03-12 07:55:49 -0400

    plus

--- projects/aJSFv22DWM40Servlet40JPAv22_202401241048/src/main/java/pkgs/servletFilters/Teste01ServletFilter.java
+++ projects/aJSFv22DWM40Servlet40JPAv22_202401241048/src/main/java/pkgs/servletFilters/Teste01ServletFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 
 import pkgs.utils.JSFUtil;
 
@@ -29,6 +30,10 @@ public class Teste01ServletFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		System.out.println("Teste01ServletFilter.doFilter()");
+
+		String url = ((HttpServletRequest)request).getRequestURL().toString();
+		String queryString = ((HttpServletRequest)request).getQueryString();
+		System.out.println("[url=" + (url) + "][queryString=" + (queryString) + "]");
 
 		chain.doFilter(request, response);
 
